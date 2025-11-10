d2b02129f2f3cc712c37c35e6b5b8a011580fdd4
QingAren <okk@okk.com>
2025-11-10 23:04:38 +0800

    fix: add not null in intercept

--- src/main/java/interceptor/LoggingInterceptor.java
+++ src/main/java/interceptor/LoggingInterceptor.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
 @Slf4j
 public class LoggingInterceptor implements Interceptor {
     @Override
+    @NotNull
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
         
